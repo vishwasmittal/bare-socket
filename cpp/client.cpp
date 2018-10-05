e314a9239fe7c6eaba059f029c7ddc3c00fb6be3
Aditya Gupta <adi1998.2010@gmail.com>
2018-10-05 21:30:22 +0530

    Add HTTP GET request

--- cpp/client.cpp
+++ cpp/client.cpp
@@ -12,7 +12,10 @@ int main(int argc, char const *argv[])
     struct sockaddr_in address; 
     int sock = 0, valread; 
     struct sockaddr_in serv_addr; 
-    char *hello = "Hello from client"; 
+    char *hello = "GET / HTTP/1.1\r\n"
+                  "Host: localhost:8000\r\n"
+                  "Accept: */*\r\n"
+                  "\r\n"; 
     char buffer[1024] = {0}; 
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
     { 
